3f49c47f52fd81704aada22fb27640d0f7691ad6
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-04-24 09:57:44 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.js
@@ -0,0 +1,10 @@
+// Last updated: 24/04/2026, 09:57:39
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
