767bc9f4414d76d71445ca236544ae0e23fced28
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-04-24 09:57:22 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 24/04/2026, 09:57:19
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+
+        int k = 1; // first element is unique
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[i-1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
