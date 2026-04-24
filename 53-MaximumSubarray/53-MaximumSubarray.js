85d52ce4eef730f414cd6fbfd00a58f97047fb8c
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-04-24 09:56:56 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.js
@@ -0,0 +1,16 @@
+// Last updated: 24/04/2026, 09:56:54
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let currentSum = nums[0];
+    let maxSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+};
