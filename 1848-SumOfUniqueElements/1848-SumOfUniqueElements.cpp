4ac2871e5b2f4da7911d2340de4f13f643d4d146
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-05-07 10:09:05 +0530

    Create 1848-SumOfUniqueElements

---
+++ 1848-SumOfUniqueElements/1848-SumOfUniqueElements.cpp
@@ -0,0 +1,21 @@
+// Last updated: 07/05/2026, 10:09:01
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+
+        int sum = 0;
+
+        for (auto it : m) {
+            if (it.second == 1) {
+                sum += it.first;
+            }
+        }
+
+        return sum;
+    }
+};
