a7d6798066b56f628529a0e7544d1158d87b3a13
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-05-07 10:09:21 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.cpp
@@ -0,0 +1,17 @@
+// Last updated: 07/05/2026, 10:09:18
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> s(jewels.begin(), jewels.end());
+        
+        int count = 0;
+        for (char c : stones) {
+        if (s.count(c)) {
+            count++;
+            }
+
+        }
+        
+        return count;
+    }
+};
