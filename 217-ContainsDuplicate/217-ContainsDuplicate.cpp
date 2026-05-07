944d5d19a673597fd50663423330645e9b370885
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-05-07 19:46:37 +0530

    File updated at 07/05/2026, 19:46:31

--- 217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,19 +1,17 @@
-// Last updated: 24/04/2026, 09:56:41
-#include <unordered_set>
-using namespace std;
-
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s;
-
-        for(int x : nums) {
-            if(s.count(x)) {
-                return true;
-            }
-            s.insert(x);
-        }
-
-        return false;
-    }
-};
+// Last updated: 07/05/2026, 19:46:31
+1#include <unordered_set>
+2using namespace std;
+3
+4class Solution {
+5public:
+6    bool containsDuplicate(vector<int>& nums) {
+7      unordered_set<int> s;
+8      for(int num : nums){
+9        if(s.count(num))
+10        return true;
+11
+12        s.insert(num);
+13      }
+14      return false;
+15    }
+16};
