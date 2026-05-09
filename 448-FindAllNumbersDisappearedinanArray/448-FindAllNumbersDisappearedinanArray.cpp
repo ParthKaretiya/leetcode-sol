6a8a77530cd3f6fce195c45212e058506f1efca5
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-05-09 10:36:25 +0530

    Create 448-FindAllNumbersDisappearedinanArray

---
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 09/05/2026, 10:36:22
+1class Solution {
+2public:
+3    vector<int> findDisappearedNumbers(vector<int>& nums) {
+4        unordered_set<int> st;
+5        for(int x : nums){
+6            st.insert(x);
+7        }
+8        vector<int>ans;
+9        for(int i=1;i<=nums.size();i++){
+10            if(st.count(i)==0){
+11                ans.push_back(i);
+12            }
+13        }
+14        return ans;
+15    }
+16};
