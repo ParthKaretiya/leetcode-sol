5a129bb25e3bd641797de00b39e282f94004130b
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-05-10 20:13:13 +0530

    File updated at 10/05/2026, 20:13:13

--- 7-ReverseInteger/7-ReverseInteger.cpp
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -1,21 +1,21 @@
-// Last updated: 24/04/2026, 09:57:35
-class Solution {
-public:
-    int reverse(int x) {
-        int sum = 0;
-
-        while (x != 0) {
-            int rem = x % 10;
-
-           
-            if (sum > INT_MAX / 10 || sum < INT_MIN / 10) {
-                return 0;
-            }
-
-            sum = sum * 10 + rem;
-            x /= 10;
-        }
-
-        return sum;
-    }
-};
+// Last updated: 10/05/2026, 20:13:13
+1class Solution {
+2public:
+3    int reverse(int x) {
+4        int sum = 0;
+5
+6        while (x != 0) {
+7            int rem = x % 10;
+8
+9           
+10            if (sum > INT_MAX / 10 || sum < INT_MIN / 10) {
+11                return 0;
+12            }
+13
+14            sum = sum * 10 + rem;
+15            x /= 10;
+16        }
+17
+18        return sum;
+19    }
+20};
