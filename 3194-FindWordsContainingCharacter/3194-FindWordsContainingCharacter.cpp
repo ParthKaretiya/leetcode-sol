29b6438f6eb1355d5460096f5c812ca7e7d484b6
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-05-21 11:07:36 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.cpp
@@ -0,0 +1,18 @@
+// Last updated: 21/05/2026, 11:07:35
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+
+        for (int i = 0; i < words.size(); i++) {
+            for (char ch : words[i]) {
+                if (ch == x) {
+                    ans.push_back(i);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
