b6dc7d56f8dfba6823ebb218089a0c1e28b2e654
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-06-09 15:51:40 +0530

    File updated at 09/06/2026, 15:51:38

--- 125-ValidPalindrome/125-ValidPalindrome.cpp
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -1,37 +1,37 @@
-// Last updated: 24/04/2026, 09:56:45
-class Solution {
-public:
-    bool isPalindrome(string s) {
-        string temp = "";
-
-        // for loop to keep only letters and digits
-        for (char ch : s) {
-            if ((ch >= 'a' && ch <= 'z') ||
-                (ch >= 'A' && ch <= 'Z') ||
-                (ch >= '0' && ch <= '9')) {
-
-                // convert uppercase to lowercase
-                if (ch >= 'A' && ch <= 'Z') {
-                    ch = ch + 32;
-                }
-
-                temp += ch;
-            }
-        }
-
-        // Two pointers using while loop
-        int left = 0;
-        int right = temp.length() - 1;
-
-        while (left < right) {
-            if (temp[left] != temp[right]) {
-                return false;
-            }
-
-            left++;
-            right--;
-        }
-
-        return true;
-    }
-};
+// Last updated: 09/06/2026, 15:51:38
+1class Solution {
+2public:
+3    bool isPalindrome(string s) {
+4        string temp = "";
+5
+6        // for loop to keep only letters and digits
+7        for (char ch : s) {
+8            if ((ch >= 'a' && ch <= 'z') ||
+9                (ch >= 'A' && ch <= 'Z') ||
+10                (ch >= '0' && ch <= '9')) {
+11
+12                // convert uppercase to lowercase
+13                if (ch >= 'A' && ch <= 'Z') {
+14                    ch = ch + 32;
+15                }
+16
+17                temp += ch;
+18            }
+19        }
+20
+21        // Two pointers using while loop
+22        int left = 0;
+23        int right = temp.length() - 1;
+24
+25        while (left < right) {
+26            if (temp[left] != temp[right]) {
+27                return false;
+28            }
+29
+30            left++;
+31            right--;
+32        }
+33
+34        return true;
+35    }
+36};
