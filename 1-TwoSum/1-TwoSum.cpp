2e4d6881d961013d2ce29fa3c0fad9436e6e91b8
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-06-11 01:26:48 +0530

    File updated at 11/06/2026, 01:26:44

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,21 +1,19 @@
-// Last updated: 24/04/2026, 09:57:38
-
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp; 
-
-        for(int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-
-            
-            if(mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
-            }
-            
-            mp[nums[i]] = i;
-        }
-
-        return {};
-    }
-};
+// Last updated: 11/06/2026, 01:26:44
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        unordered_map<int, int> mp;
+5
+6        for (int i = 0; i < nums.size(); i++) {
+7            int rem = target - nums[i];
+8
+9            if (mp.find(rem) != mp.end()) {
+10                return {mp[rem], i};
+11            }
+12
+13            mp[nums[i]] = i;
+14        }
+15
+16        return {};
+17    }
+18};
