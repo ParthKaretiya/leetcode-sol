e5da62ad7ba32445dda07471e98fd410939548e1
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-06-14 14:45:03 +0530

    File updated at 14/06/2026, 14:45:02

--- 905-SortArrayByParity/905-SortArrayByParity.cpp
+++ 905-SortArrayByParity/905-SortArrayByParity.cpp
@@ -1,21 +1,20 @@
-// Last updated: 07/06/2026, 22:03:18
+// Last updated: 14/06/2026, 14:45:02
 1class Solution {
 2public:
 3    vector<int> sortArrayByParity(vector<int>& nums) {
-4        vector<int> even;
-5        vector<int> odd;
-6
-7        for(int num : nums) {
-8            if(num % 2 == 0) {
-9                even.push_back(num);
-10            }
-11            else {
-12                odd.push_back(num);
-13            }
+4       vector<int>ans ;
+5       for(int x : nums){
+6        if(x % 2  == 0){
+7            ans.push_back(x);
+8        }
+9       }
+10
+11       for(int x : nums){
+12        if(x%2!= 0){
+13            ans.push_back(x);
 14        }
-15
-16        even.insert(even.end(), odd.begin(), odd.end());
-17
-18        return even;
-19    }
-20};
+15       }
+16
+17       return ans;
+18    }
+19};
