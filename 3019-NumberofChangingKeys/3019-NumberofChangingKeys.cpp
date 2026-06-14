5f18fc7190182ea62378b0d035259bf718c69fe9
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-06-14 17:01:20 +0530

    File updated at 14/06/2026, 17:01:18

--- 3019-NumberofChangingKeys/3019-NumberofChangingKeys.cpp
+++ 3019-NumberofChangingKeys/3019-NumberofChangingKeys.cpp
@@ -1,18 +1,14 @@
-// Last updated: 06/06/2026, 13:32:50
+// Last updated: 14/06/2026, 17:01:18
 1class Solution {
 2public:
 3    int countKeyChanges(string s) {
-4
-5        int count = 0;
-6
-7        for (int i = 1; i < s.size(); i++) {
-8
-9            // convert both to lowercase
-10            if (tolower(s[i]) != tolower(s[i - 1])) {
-11                count++;
-12            }
-13        }
-14
-15        return count;
-16    }
-17};
+4        int sum = 0;
+5
+6        for(int i = 1; i < s.size(); i++) {
+7            if(tolower(s[i]) != tolower(s[i - 1]))
+8                sum++;
+9        }
+10
+11        return sum;
+12    }
+13};
