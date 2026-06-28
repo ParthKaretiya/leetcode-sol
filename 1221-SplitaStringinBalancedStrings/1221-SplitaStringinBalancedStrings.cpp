e6581b61baebbb1648696d6def01f06d81c22aea
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-06-28 17:20:32 +0530

    File updated at 28/06/2026, 17:20:27

--- 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.cpp
+++ 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.cpp
@@ -1,22 +1,22 @@
-// Last updated: 29/05/2026, 16:20:38
+// Last updated: 28/06/2026, 17:20:27
 1class Solution {
 2public:
 3    int balancedStringSplit(string s) {
-4        int balance = 0;
-5        int count = 0;
+4     int count = 0;
+5     int balance = 0;
 6
-7        for(char ch : s) {
-8            if(ch == 'L') {
-9                balance++;
-10            } else {
-11                balance--;
-12            }
-13
-14            if(balance == 0) {
-15                count++;
-16            }
+7     for(char ch :s ){
+8        if(ch == 'L'){
+9            balance ++;
+10        }
+11        else{
+12            balance --;
+13        }
+14
+15        if(balance == 0){
+16            count ++;
 17        }
-18
-19        return count;
+18     }  
+19     return count; 
 20    }
 21};
