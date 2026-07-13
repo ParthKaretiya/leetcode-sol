a66c1e2016c9641e52d8537cda68d8b4a3d5ee75
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-07-13 13:18:02 +0530

    Create 1797-GoalParserInterpretation

---
+++ 1797-GoalParserInterpretation/1797-GoalParserInterpretation.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/13/2026, 1:18:04 PM
+class Solution {
+public:
+    string interpret(string command) {
+        string ans = "";
+
+        for (int i = 0; i < command.size(); i++) {
+            if (command[i] == 'G') {
+                ans += 'G';
+            } 
+            else if (command[i] == '(' && command[i + 1] == ')') {
+                ans += 'o';
+                i++;
+            } 
+            else {
+                ans += "al";
+                i += 3;
+            }
+        }
+
+        return ans;
+    }
+};
