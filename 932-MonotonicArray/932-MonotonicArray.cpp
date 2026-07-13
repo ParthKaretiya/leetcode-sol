9c232ce4dce31b44fea5dd7bea7798b2ec0d7153
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-07-13 13:20:13 +0530

    Create 932-MonotonicArray

---
+++ 932-MonotonicArray/932-MonotonicArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/13/2026, 1:20:16 PM
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool inc = true, dec = true;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1])
+                dec = false;
+
+            if (nums[i] < nums[i - 1])
+                inc = false;
+        }
+
+        return inc || dec;
+    }
+};
