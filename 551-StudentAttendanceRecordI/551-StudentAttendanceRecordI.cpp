d3931ca349b452cb06012bd5bace36823a0d67ac
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-07-15 17:25:39 +0530

    File updated at 7/15/2026, 5:25:42 PM

--- 551-StudentAttendanceRecordI/551-StudentAttendanceRecordI.cpp
+++ 551-StudentAttendanceRecordI/551-StudentAttendanceRecordI.cpp
@@ -1,26 +1,26 @@
-// Last updated: 7/13/2026, 1:21:24 PM
-class Solution {
-public:
-    bool checkRecord(string s) {
-        int absent = 0;
-        int late = 0;
-
-        for (char c : s) {
-            if (c == 'A') {
-                absent++;
-                late = 0;
-            } 
-            else if (c == 'L') {
-                late++;
-            } 
-            else {
-                late = 0;
-            }
-
-            if (absent >= 2 || late >= 3)
-                return false;
-        }
-
-        return true;
-    }
-};
+// Last updated: 7/15/2026, 5:25:42 PM
+1class Solution {
+2public:
+3    bool checkRecord(string s) {
+4        int absent = 0;
+5        int late = 0;
+6
+7        for (char c : s) {
+8            if (c == 'A') {
+9                absent++;
+10                late = 0;
+11            } 
+12            else if (c == 'L') {
+13                late++;
+14            } 
+15            else {
+16                late = 0;
+17            }
+18
+19            if (absent >= 2 || late >= 3)
+20                return false;
+21        }
+22
+23        return true;
+24    }
+25};
