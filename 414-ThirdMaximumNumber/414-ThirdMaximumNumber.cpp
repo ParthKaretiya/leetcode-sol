a4a9e16303c838d8f6837cfa22a5a54ee372c98a
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-07-17 13:09:56 +0530

    File updated at 7/17/2026, 1:10:00 PM

--- 414-ThirdMaximumNumber/414-ThirdMaximumNumber.cpp
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.cpp
@@ -1,18 +1,18 @@
-// Last updated: 7/13/2026, 1:22:02 PM
-class Solution {
-public:
-    int thirdMax(vector<int>& nums) {
-        set<long long> s;
-
-        for (int num : nums) {
-            s.insert(num);
-            if (s.size() > 3)
-                s.erase(s.begin());
-        }
-
-        if (s.size() == 3)
-            return *s.begin();
-
-        return *s.rbegin();
-    }
-};
+// Last updated: 7/17/2026, 1:10:00 PM
+1class Solution {
+2public:
+3    int thirdMax(vector<int>& nums) {
+4        set<long long> s;
+5
+6        for (int num : nums) {
+7            s.insert(num);
+8            if (s.size() > 3)
+9                s.erase(s.begin());
+10        }
+11
+12        if (s.size() == 3)
+13            return *s.begin();
+14
+15        return *s.rbegin();
+16    }
+17};
