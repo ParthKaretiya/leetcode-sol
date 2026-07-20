1637d4b125e3ac2e881aa494fdcf02dfeeba7111
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-07-20 09:13:58 +0530

    File updated at 7/20/2026, 9:13:59 AM

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,22 +1,22 @@
-// Last updated: 24/04/2026, 09:57:00
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-        unordered_map<string, vector<string>> m;
-
-        for(string word : strs){
-            string temp = word;
-            sort(temp.begin(), temp.end());
-            m[temp].push_back(word);
-        }
-
-        vector<vector<string>> result;
-
-        for(auto &it : m){
-            result.push_back(it.second);
-        }
-
-        return result;
-    }
-};
+// Last updated: 7/20/2026, 9:13:59 AM
+1class Solution {
+2public:
+3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+4        
+5        unordered_map<string, vector<string>> m;
+6
+7        for(string word : strs){
+8            string temp = word;
+9            sort(temp.begin(), temp.end());
+10            m[temp].push_back(word);
+11        }
+12
+13        vector<vector<string>> result;
+14
+15        for(auto &it : m){
+16            result.push_back(it.second);
+17        }
+18
+19        return result;
+20    }
+21};
