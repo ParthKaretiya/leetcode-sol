b81ca2593e8ff44646135ae52ec209c6c2745580
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-07-30 13:34:13 +0530

    Create 443-StringCompression

---
+++ 443-StringCompression/443-StringCompression.cpp
@@ -0,0 +1,31 @@
+// Last updated: 30/07/2026, 13:34:13
+1class Solution {
+2public:
+3    int compress(vector<char>& chars) {
+4
+5        vector<char> ans;
+6
+7        int count = 1;
+8
+9        for (int i = 0; i < chars.size(); i++) {
+10            if (i + 1 < chars.size() && chars[i] == chars[i + 1]) {
+11                count++;
+12            } else {
+13                ans.push_back(chars[i]);
+14
+15                if (count > 1) {
+16                    string s = to_string(count);
+17
+18                    for (char c : s) {
+19                        ans.push_back(c);
+20                    }
+21                }
+22                count  = 1;
+23            }
+24        }
+25
+26        chars = ans;
+27
+28        return chars.size();
+29    }
+30};
