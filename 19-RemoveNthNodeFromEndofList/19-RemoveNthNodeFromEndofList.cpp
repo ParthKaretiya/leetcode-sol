a4ab319d8598ba18ab489f8420795145aab4c101
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-08-05 10:39:55 +0530

    File updated at 8/5/2026, 10:39:57 AM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.cpp
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.cpp
@@ -1,23 +1,41 @@
-// Last updated: 8/5/2026, 10:39:21 AM
+// Last updated: 8/5/2026, 10:39:57 AM
 1class Solution {
 2public:
-3    ListNode* removeNthFromEnd(ListNode* head, int n) {
-4        ListNode dummy(0);
-5        dummy.next = head;
-6
-7        ListNode* fast = &dummy;
-8        ListNode* slow = &dummy;
-9
-10        for (int i = 0; i <= n; i++)
-11            fast = fast->next;
-12
-13        while (fast) {
-14            fast = fast->next;
-15            slow = slow->next;
-16        }
-17
-18        slow->next = slow->next->next;
-19
-20        return dummy.next;
-21    }
-22};
+3    bool isNumber(string s) {
+4        int i = 0, n = s.size();
+5
+6        while (i < n && s[i] == ' ')
+7            i++;
+8
+9        if (i < n && (s[i] == '+' || s[i] == '-'))
+10            i++;
+11
+12        bool num = false, dot = false, exp = false;
+13
+14        while (i < n) {
+15            if (isdigit(s[i])) {
+16                num = true;
+17            } else if (s[i] == '.') {
+18                if (dot || exp)
+19                    return false;
+20                dot = true;
+21            } else if (s[i] == 'e' || s[i] == 'E') {
+22                if (exp || !num)
+23                    return false;
+24                exp = true;
+25                num = false;
+26                if (i + 1 < n && (s[i + 1] == '+' || s[i + 1] == '-'))
+27                    i++;
+28            } else if (s[i] == ' ') {
+29                while (i < n && s[i] == ' ')
+30                    i++;
+31                return i == n && num;
+32            } else {
+33                return false;
+34            }
+35            i++;
+36        }
+37
+38        return num;
+39    }
+40};
