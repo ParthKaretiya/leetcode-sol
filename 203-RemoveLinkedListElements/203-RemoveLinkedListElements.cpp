44a41600b12912d526eef1b2424f431c2aed959c
Parth-Karetiya <parth.karetiya.cg@gmail.com>
2026-08-09 15:38:26 +0530

    File updated at 8/9/2026, 3:38:27 PM

--- 203-RemoveLinkedListElements/203-RemoveLinkedListElements.cpp
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.cpp
@@ -1,23 +1,41 @@
-// Last updated: 8/9/2026, 3:37:12 PM
+// Last updated: 8/9/2026, 3:38:27 PM
 1class Solution {
 2public:
-3    ListNode* removeElements(ListNode* head, int val) {
+3    bool isPalindrome(ListNode* head) {
 4
-5        ListNode* dummy = new ListNode(0);
-6        dummy->next = head;
-7
-8        ListNode* temp = dummy;
-9
-10        while (temp->next != nullptr) {
-11
-12            if (temp->next->val == val) {
-13                temp->next = temp->next->next;
-14            }
-15            else {
-16                temp = temp->next;
-17            }
-18        }
-19
-20        return dummy->next;
-21    }
-22};
+5        // Find middle
+6        ListNode* slow = head;
+7        ListNode* fast = head;
+8
+9        while (fast != nullptr && fast->next != nullptr) {
+10            slow = slow->next;
+11            fast = fast->next->next;
+12        }
+13
+14        // Reverse second half
+15        ListNode* prev = nullptr;
+16        ListNode* curr = slow;
+17
+18        while (curr != nullptr) {
+19            ListNode* next = curr->next;
+20            curr->next = prev;
+21            prev = curr;
+22            curr = next;
+23        }
+24
+25        // Compare first half and reversed second half
+26        ListNode* left = head;
+27        ListNode* right = prev;
+28
+29        while (right != nullptr) {
+30            if (left->val != right->val) {
+31                return false;
+32            }
+33
+34            left = left->next;
+35            right = right->next;
+36        }
+37
+38        return true;
+39    }
+40};
